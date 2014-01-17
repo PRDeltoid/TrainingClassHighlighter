556ad68f817fe5af4dcaa620698109b96a794d2d
Taylor Britton <tbrit10@gmail.com>
2014-01-17 11:23:11 -0800

    Made button clicks functional
    -Made clicking filter buttons functional instead of programmed per case.
    Now the ID determines the TYPE of filter (day, time, other), and the
    value determines which filter to use under that type (ie. value="0"
    where 0 corresponds to the position of the Monday filter object in the
    array).
    -Moved filter array initialization into it's own function.
    -Beginning overhaul of determineHighlightFilters function (currently
    incredibly convoluted).

--- classHighlighter.js
+++ classHighlighter.js
@@ -5,7 +5,6 @@ var timeFilterObjArray = [];
 var otherFilterObj;
 var otherFilterObjArray = [];
 
-//object constructors
 function dayFilterObj(stateIn,filterValueIn) {
 	this.state = stateIn;
 	this.filterValue = filterValueIn;
@@ -21,184 +20,17 @@ function otherFilterObj(stateIn,filterValueIn) {
 	this.filterValue = filterValueIn;
 }
 
-//Initialize day and time filters with days of the week and class times. Default state is false for all flags
-dayFilterObjArray.push(new dayFilterObj(false,".monday"));
-dayFilterObjArray.push(new dayFilterObj(false,".tuesday"));
-dayFilterObjArray.push(new dayFilterObj(false,".wednesday"));
-dayFilterObjArray.push(new dayFilterObj(false,".thursday"));
-dayFilterObjArray.push(new dayFilterObj(false,".friday"));
-dayFilterObjArray.push(new dayFilterObj(false,".saturday"));
-//dayFilterObjArray.push(new dayFilterObj(false,".sunday"));
-
-timeFilterObjArray.push(new timeFilterObj(false,".1000"));
-timeFilterObjArray.push(new timeFilterObj(false,".1115"));
-timeFilterObjArray.push(new timeFilterObj(false,".615"));
-timeFilterObjArray.push(new timeFilterObj(false,".730"));
-
-
-otherFilterObjArray.push(new otherFilterObj(false,".smallbreed"));
-otherFilterObjArray.push(new otherFilterObj(false,".senior"));
-
-$(document).ready(function(){ 
-	//Highlight 10:00 classes
-	$('#highlight1000').on('click', function() { 
-		if(timeFilterObjArray[0].state == true) {
-			timeFilterObjArray[0].state = false;			
-			$(".activeFilterButton").filter("#highlight1000").removeClass("activeFilterButton");
-		} else {
-			timeFilterObjArray[0].state = true;
-			$(".filterButton").filter("#highlight1000").addClass("activeFilterButton");			
-		}		
-		wipeHighlights();		
-		$(determineHighlightFilters()).addClass("highlightClass");	
-	}); 
-
-	//Highlight 11:15 classes
-	$('#highlight1115').on('click', function() { 
-		if(timeFilterObjArray[1].state == true) {
-			timeFilterObjArray[1].state = false;			
-			$(".activeFilterButton").filter("#highlight1115").removeClass("activeFilterButton");
-		} else {
-			timeFilterObjArray[1].state = true;
-			$(".filterButton").filter("#highlight1115").addClass("activeFilterButton");			
-		}		
-		wipeHighlights();		
-		$(determineHighlightFilters()).addClass("highlightClass");	
-	}); 
-
-	//Highlight 6:15 classes
-	$('#highlight615').on('click', function() { 
-		if(timeFilterObjArray[2].state == true) {
-			timeFilterObjArray[2].state = false;			
-			$(".activeFilterButton").filter("#highlight615").removeClass("activeFilterButton");
-		} else {
-			timeFilterObjArray[2].state = true;
-			$(".filterButton").filter("#highlight615").addClass("activeFilterButton");			
-		}		
-		wipeHighlights();		
-		$(determineHighlightFilters()).addClass("highlightClass");	
-	}); 
+$(document).ready(function(){ 	
+	initializeFilterArrays();
 	
-	//Highlight 7:30 classes
-	$('#highlight730').on('click', function() { 
-		if(timeFilterObjArray[3].state == true) {
-			timeFilterObjArray[3].state = false;			
-			$(".activeFilterButton").filter("#highlight730").removeClass("activeFilterButton");
-		} else {
-			timeFilterObjArray[3].state = true;
-			$(".filterButton").filter("#highlight730").addClass("activeFilterButton");
-		}	
+	$('.filterButton').on('click',function() {		
+		changeFilterState(this);
 		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	}); 
-
-//highlight smallbreed classes	
-	$('#highlightSmallbreedOnly').on('click', function() { 
-		if(otherFilterObjArray[0].state == true) {
-			otherFilterObjArray[0].state = false;			
-			$(".activeFilterButton").filter("#highlightSmallbreedOnly").removeClass("activeFilterButton");
-		} else {
-			otherFilterObjArray[0].state = true;
-			$(".filterButton").filter("#highlightSmallbreedOnly").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	}); 
-	
-	//highlight senior classes
-	$('#highlightSeniorOnly').on('click', function() { 
-		if(otherFilterObjArray[1].state == true) {
-			otherFilterObjArray[1].state = false;			
-			$(".activeFilterButton").filter("#highlightSeniorOnly").removeClass("activeFilterButton");
-		} else {
-			otherFilterObjArray[1].state = true;
-			$(".filterButton").filter("#highlightSeniorOnly").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	}); 
-
-	//Highlight Monday clicked
-	$('#highlightMonday').on('click', function() { 
-		if(dayFilterObjArray[0].state == true) {
-			dayFilterObjArray[0].state = false;
-			$(".activeFilterButton").filter("#highlightMonday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[0].state = true;
-			$(".filterButton").filter("#highlightMonday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	}); 
-	
-	//Highlight Tuesday clicked
-	$('#highlightTuesday').on('click', function() { 
-		if(dayFilterObjArray[1].state == true) {
-			dayFilterObjArray[1].state = false;			
-			$(".activeFilterButton").filter("#highlightTuesday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[1].state = true;
-			$(".filterButton").filter("#highlightTuesday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	});
-		//Highlight Wednesday clicked
-	$('#highlightWednesday').on('click', function() { 
-		if(dayFilterObjArray[2].state == true) {
-			dayFilterObjArray[2].state = false;			
-			$(".activeFilterButton").filter("#highlightWednesday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[2].state = true;
-			$(".filterButton").filter("#highlightWednesday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	});
-		//Highlight Thursday clicked
-	$('#highlightThursday').on('click', function() { 
-		if(dayFilterObjArray[3].state == true) {
-			dayFilterObjArray[3].state = false;			
-			$(".activeFilterButton").filter("#highlightThursday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[3].state = true;
-			$(".filterButton").filter("#highlightThursday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	});
-		//Highlight Friday clicked
-	$('#highlightFriday').on('click', function() { 
-		if(dayFilterObjArray[4].state == true) {
-			dayFilterObjArray[4].state = false;			
-			$(".activeFilterButton").filter("#highlightFriday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[4].state = true;
-			$(".filterButton").filter("#highlightFriday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	});
-		//Highlight Saturday clicked
-	$('#highlightSaturday').on('click', function() { 
-		if(dayFilterObjArray[5].state == true) {
-			dayFilterObjArray[5].state = false;			
-			$(".activeFilterButton").filter("#highlightSaturday").removeClass("activeFilterButton");
-		} else {
-			dayFilterObjArray[5].state = true;
-			$(".filterButton").filter("#highlightSaturday").addClass("activeFilterButton");
-		}	
-		wipeHighlights();
-		$(determineHighlightFilters()).addClass("highlightClass");			
-	});
+		$(determineHighlightFilters()).addClass("highlightClass");
+	});	
 });
 
-
-//////////////////////
-//Function to determine filter value for class highlighting
-//////////////////////
-
-function determineHighlightFilters() {
+/*function determineHighlightFilters() {
 
 	var filterText = "";
 	var dayFilterCounter = 0;
@@ -278,25 +110,78 @@ function determineHighlightFilters() {
 		}
 	}
 	
-	alert(filterText);
+	console.log(filterText);
 	return filterText;
+}*/
+
+function determineHighlightFilters() {
+	var filterValueArray = [];
+	var compoundFilterValue = "";
+	
+	for(var dayIterator=0;dayIterator<dayFilterObjArray.length;dayIterator++) {
+		if(dayFilterObjArray[dayIterator].state==true) {
+			for(var timeIterator=0;timeIterator<timeFilterObjArray.length;timeIterator++) {				
+				if(timeFilterObjArray[dayIterator].state==true) {
+					filterValueArray[dayIterator] = dayFilterObjArray[dayIterator].filterValue;
+				}
+			}
+		}
+	}
+	
+	console.log(compoundFilterValue);
+	return compoundFilterValue;
+}
+
+function checkTimeFilterSet() {
+	//arrayLength = timeFilterObjArray.length-1;
+	for(var it=0;it<timeFilterObjArray.length;it++) {
+		if(timeFilterObjArray[it].state == true){	return true;	} 
+	}	
+	return false;
 }
 
 function wipeHighlights() {
 	$(".highlightClass").removeClass("highlightClass");
 }
 
-function checkTimeFilterSet() {
-	timeFilterSet = false;
-	arrayLength = timeFilterObjArray.length-1;
-
-	for(var it=0;it<arrayLength;it++) {
-		if(timeFilterObjArray[it].state == true){
-			timeFilterSet = true;
-		}
+function changeFilterState(buttonClicked) {
+	var value = buttonClicked.value;
+	switch(buttonClicked.id) {
+		case 'dayButton':
+			dayFilterObjArray[value].state = !dayFilterObjArray[value].state;
+			console.log(dayFilterObjArray[value]);
+			break;
+		case 'timeButton':
+			timeFilterObjArray[value].state = !timeFilterObjArray[value].state;
+			console.log(timeFilterObjArray[value]);
+			break;
+		case 'otherButton':
+			otherFilterObjArray[value].state = !otherFilterObjArray[value].state;
+			console.log(otherFilterObjArray[value]);
+			break;
+		default: 
+			console.log('Error: Unknown Button Type Selected');
+			break;
 	}
-	return timeFilterSet;
-
+	return;	
 }
 
+function initializeFilterArrays() {
+	//Initialize day and time filters with days of the week and class times. Default state is false for all flags
+	dayFilterObjArray.push(new dayFilterObj(false,".monday"));
+	dayFilterObjArray.push(new dayFilterObj(false,".tuesday"));
+	dayFilterObjArray.push(new dayFilterObj(false,".wednesday"));
+	dayFilterObjArray.push(new dayFilterObj(false,".thursday"));
+	dayFilterObjArray.push(new dayFilterObj(false,".friday"));
+	dayFilterObjArray.push(new dayFilterObj(false,".saturday"));
+	
+	timeFilterObjArray.push(new timeFilterObj(false,".1000"));
+	timeFilterObjArray.push(new timeFilterObj(false,".1115"));
+	timeFilterObjArray.push(new timeFilterObj(false,".615"));
+	timeFilterObjArray.push(new timeFilterObj(false,".730"));
+	
+	
+	otherFilterObjArray.push(new otherFilterObj(false,".smallbreed"));
+	otherFilterObjArray.push(new otherFilterObj(false,".senior"));
+}
 
